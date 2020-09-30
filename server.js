11c32e0ea88e6ba93a665e6aea317490b65a50cc
Christof <bauer.christof@gmail.com>
2020-09-30 16:20:15 +0545

    Fix backend. use bodyParser and json response!

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
@@ -12,6 +13,8 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.static(__dirname + '/dist'));
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // Mongoose Connect
 mongoose.connect(
