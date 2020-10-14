76110c813401815a36ec0cec5480b14c23f37b20
Christof <bauer.christof@gmail.com>
2020-10-14 02:10:08 -0500

    Make JWT expire after 1 week.

--- src/routes/users.js
+++ src/routes/users.js
@@ -61,12 +61,13 @@ router.post("/login", async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1w' });
     res.json({
       token,
       user: {
         id: user._id,
         displayName: user.displayName,
+        email: user.displayName
       },
     });
   } catch (err) {
