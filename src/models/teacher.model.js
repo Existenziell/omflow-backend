4757dfa85cca4d4912f44927bfaf260332aef107
Christof <bauer.christof@gmail.com>
2020-10-16 23:29:28 -0500

    Adaptations for DB update (bio, quote, instragram, pose)

--- src/models/teacher.model.js
+++ src/models/teacher.model.js
@@ -3,13 +3,16 @@ const { Schema, model } = require('mongoose');
 const teacherSchema = new Schema({
   name: { type: String, required: true, unique: true, trim: true },
   description: { type: String, trim: true },
+  quote: { type: String, trim: true },
+  instragram: { type: String, trim: true },
+  pose: { type: String, trim: true },
   address: { type: String, trim: true },
+  coordinates: { type: Array },
+  image: { type: String },
+  video: { type: String },
   tag: { type: String, unique: true },
   levels: { type: Array },
   styles: { type: Array },
-  image: { type: String },
-  video: { type: String },
-  coordinates: { type: Array },
   practices: [{ type: Schema.Types.ObjectId, ref: 'Practice' }],
   userId: { type: Schema.Types.ObjectId, ref: 'User' }
 }, {
